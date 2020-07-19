38755b46fdfb9a34ac5666a7e25384c44d891460
Max Ainatchi <maxwell.ainatchi@gmail.com>
2020-07-18 22:46:09 -0400

    basic app

---
+++ barber/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  barber
+//
+//  Created by Max Ainatchi on 7/18/20.
+//  Copyright © 2020 Max Ainatchi, Inc. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let contentView = ContentView()
+
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 400)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "icon")
+            button.action = #selector(self.togglePopover(_:))
+        }
+    }
+
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+                NSApp.activate(ignoringOtherApps: true)
+                self.popover.contentViewController?.view.window?.becomeKey()
+            }
+        }
+    }
+}
---
+++ barber/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  barber
+//
+//  Created by Max Ainatchi on 7/18/20.
+//  Copyright © 2020 Max Ainatchi, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
